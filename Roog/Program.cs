260b8f0d8b42da44c74bd244b222e27b44efe66d
Sinawo <gcobanimayoyo@gmail.com>
2019-02-15 18:47:07 +0200

    Update Program.cs

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -6,7 +6,7 @@ namespace Roog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello g16m4204");
         }
     }
 }
